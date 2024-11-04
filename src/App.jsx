7f5f0a67c1fbdec9fb8e27d80017f4c3201dad31
Harsha vardhan <harshavardhan1617@gmail.com>
2024-11-04 15:16:54 +0530

    fix: avoid data fetch on each visit to home

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Home from "./home/Home";
 import { Route, Routes } from "react-router-dom";
 import Category from "./category/Category";
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://dummyjson.com/products/category-list"
+      );
+      const data = await response.json();
+      setData(data);
+    };
+    fetchData();
+  }, []);
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home data={data} />} />
       <Route path="/:category" element={<Category />} />
     </Routes>
   );
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -2,18 +2,7 @@ import { useEffect, useState } from "react";
 import CategoryList from "../category-list/CategoryList";
 import { Link } from "react-router-dom";
 
-export default function Home() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(
-        "https://dummyjson.com/products/category-list"
-      );
-      const data = await response.json();
-      setData(data);
-    };
-    fetchData();
-  }, []);
+export default function Home({ data }) {
   return (
     <div className="home">
       <div className="header">#Home</div>
