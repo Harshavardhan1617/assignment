0ee2e5daa38cd5a495ee070957eb7c8f0c3ed9da
Harsha vardhan <harshavardhan1617@gmail.com>
2024-11-04 12:14:17 +0530

    instantiate components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import "./App.css";
+import Home from "./home/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
 
 export default App;
